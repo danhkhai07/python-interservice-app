9d802c7206cb3e276a0aa4e5fba11f030e6e8ee0
danhkhai07 <aliveomg@gmail.com>
2026-03-22 15:10:18 +0700

    basic api endpoints

---
+++ api/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify 
+import requests
+
+app = Flask(__name__)
+
+URL_PROCESS_JOB     = "http://worker:5001/process"
+URL_GET_JOB_INFO    = "http://worker:5001/info"
+
+@app.route("/job/<int:number>", methods=["GET"])
+def post_job(number):
+    process_res = requests.post(
+        URL_PROCESS_JOB,
+        json={"n": number},
+        timeout=2
+    )
+    taken = process_res.json().get("taken")
+    id = process_res.json().get("job_id")
+        
+    if not taken or not id:
+        return jsonify({"status": "failed"})
+    else:
+        return jsonify({"status": "taken", "job_id": id})
+        
+@app.route("/job/<int:job_id>", methods=["POST", "GET"])
+def get_job(job_id):
+    res = requests.get(
+        URL_GET_JOB_INFO,
+        json={"id": job_id},
+        timeout=2,
+    ) 
+    return jsonify(res.json())
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)   
